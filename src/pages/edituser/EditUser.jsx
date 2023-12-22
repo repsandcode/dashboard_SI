6c004e9f362624e3334125bf365706289ac9413e
repsandcode <albertjonathan.dev@gmail.com>
2023-12-22 22:40:42 +0800

    edit user update

--- src/pages/edituser/EditUser.jsx
+++ src/pages/edituser/EditUser.jsx
@@ -5,7 +5,7 @@ import { useAuth } from "../../utils/AuthContext";
 import { checkUserStatus } from "../../utils/checkUserStatus";
 import { Header, Navbar } from "../../components";
 
-const Form = () => {
+const EditUser = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
@@ -109,77 +109,70 @@ const Form = () => {
   };
 
   return (
-    <div>
-      {error && <span className="fs-4 text-danger">{error}</span>}
-
-      <form onSubmit={handleEditUser}>
-        <div className="form-group mb-4">
-          <label htmlFor="email" className="fs-4 fw-light mb-2">
-            Email:
-          </label>
-          <input
-            className="form-control p-3 fs-4 input-radius"
-            autoComplete="email"
-            value={email}
-            type="email"
-            name="email"
-            id="email"
-            onChange={handleEmailChange}
-            required
-          />
-        </div>
-
-        <div className="form-group mb-4">
-          <label htmlFor="password" className="fs-4 fw-light mb-2">
-            Password:
-          </label>
-          <input
-            className="form-control p-3 fs-4 input-radius"
-            value={password}
-            type="password"
-            name="password"
-            id="password"
-            onChange={handlePasswordChange}
-            required
-          />
-        </div>
-
-        <div className="form-group mb-4">
-          <label htmlFor="confirmPassword" className="fs-4 fw-light mb-2">
-            Confirm Password:
-          </label>
-          <input
-            className="form-control p-3 fs-4 input-radius"
-            value={confirmPassword}
-            type="password"
-            name="password"
-            id="confirmPassword"
-            onChange={handleConfirmPasswordChange}
-            required
-          />
-        </div>
-
-        <div className="pt-1">
-          <button
-            type="submit"
-            className="bg-orange border-0 input-radius text-white  w-100 p-3 fs-4"
-          >
-            Save User
-          </button>
-        </div>
-      </form>
-    </div>
-  );
-};
-
-const EditUser = () => {
-  return (
     <div className="py-4">
       <Header />
-      <Navbar page={"edit"} />
+      <Navbar page={"edit"} email={email} />
+
       <main className="row g-0">
         <div className="col-xs-8 col-sm-6 bg-white box-radius p-5">
-          <Form />
+          {error && <span className="fs-4 text-danger">{error}</span>}
+
+          <form onSubmit={handleEditUser}>
+            <div className="form-group mb-4">
+              <label htmlFor="email" className="fs-4 fw-light mb-2">
+                Email:
+              </label>
+              <input
+                className="form-control p-3 fs-4 input-radius"
+                autoComplete="email"
+                value={email}
+                type="email"
+                name="email"
+                id="email"
+                onChange={handleEmailChange}
+                required
+              />
+            </div>
+
+            <div className="form-group mb-4">
+              <label htmlFor="password" className="fs-4 fw-light mb-2">
+                Password:
+              </label>
+              <input
+                className="form-control p-3 fs-4 input-radius"
+                value={password}
+                type="password"
+                name="password"
+                id="password"
+                onChange={handlePasswordChange}
+                required
+              />
+            </div>
+
+            <div className="form-group mb-4">
+              <label htmlFor="confirmPassword" className="fs-4 fw-light mb-2">
+                Confirm Password:
+              </label>
+              <input
+                className="form-control p-3 fs-4 input-radius"
+                value={confirmPassword}
+                type="password"
+                name="password"
+                id="confirmPassword"
+                onChange={handleConfirmPasswordChange}
+                required
+              />
+            </div>
+
+            <div className="pt-1">
+              <button
+                type="submit"
+                className="bg-orange border-0 input-radius text-white  w-100 p-3 fs-4"
+              >
+                Save User
+              </button>
+            </div>
+          </form>
         </div>
       </main>
     </div>
